58ab0d7262aa45b4fd0e072f5f983f7aef8975d7
Cristian Vedovato <naitso@gmail.com>
2014-09-16 13:37:00 +0200

    Create tunnel script and readme file

---
+++ README.md
@@ -0,0 +1,38 @@
+SshPortTunneling
+==================
+
+About
+-----
+
+This script create a tunneling with local port forwarding to a remote Windows machine accessible only through work pc/work ip
+       _________          ________________          ________
+      |         |        |                |        |        |
+      | HOME PC | -----> | WORK PC TUNNEL | -----> | REMOTE |
+      |_________|        |________________|        |________|
+
+
+Require
+-------
+This require ssh installed on your local system and sshd installed running and accessible from home pc on your "work" pc
+Windows machina must have rdp enable and an firewall rule to permit access (from work)
+
+More info
+---------
+For more info about ssh tunneling please see Buddhika Chamith's post at
+http://chamibuddhika.wordpress.com/2012/03/21/ssh-tunnelling-explained/
+
+Or this article on Revsys website
+http://www.revsys.com/writings/quicktips/ssh-tunnel.html
+
+For a list of common ports please see
+http://it.wikipedia.org/wiki/Lista_di_porte_standard 
+or /etc/services in a common linux box
+
+How to use?
+-----------
+
+Save the file into your system ( "bin" path is prefeded )
+
+chmod +x ./SshPortsTunneling
+
+Now you can call the script from your CWD directory and answer the simple questions to setting the connection for your case.
---
+++ tunnel.sh
@@ -0,0 +1,69 @@
+#!/bin/sh
+#
+#####################################################
+#
+# autor: naitso
+# date: 2014/09/16
+#
+# required: ssh
+#
+# description:
+# This script create a Tunneling with Local port forwarding 
+# to connect in rdp to a Windows machine from home pc through work pc
+#
+#     -----------        ------------------        ----------
+#     | HOME PC | -----> | WORK PC TUNNEL | -----> | REMOTE |
+#     -----------        ------------------        ----------
+#
+# For more info about ssh tunneling please see Buddhika Chamith's post at
+#
+# http://chamibuddhika.wordpress.com/2012/03/21/ssh-tunnelling-explained/
+#
+# Or this article on Revsys website
+# http://www.revsys.com/writings/quicktips/ssh-tunnel.html
+#
+# For a list of common ports please see
+# http://it.wikipedia.org/wiki/Lista_di_porte_standard 
+# or /etc/services in a common linux box
+
+#####################################################
+
+#
+# Set local port, 3389 is default port for windows rdp
+LPORT=3389
+# Set remote port, 3389 is default port for rdp
+RPORT=3389
+
+clear
+echo ""
+echo "This script perform a ssh local port tunneling for rdp port 3389"
+echo ""
+echo ""
+echo "Write remote ssh server's IP or hostname to connect"
+echo "(like www.example.com or  111.222.333.444)"
+echo "this server permit the tunnel"
+echo ""
+read RHOST
+
+clear
+echo ""
+echo "Write the user to use for ssh login"
+echo ""
+read RUSER
+
+
+clear
+echo ""
+echo "Write remote host to connect to, like IP address or www.example.com"
+echo "this host is the remote pc/server to connect to"
+echo ""
+read REMOTE
+
+# -f put ssh in foreground
+# -N flag does not promt shell
+# echo "ssh -f $RUSER@$RHOST -L $LPORT:$REMOTE:$RPORT -N"
+# this command opend a ssh connection and map remote port 3389 to local port 3389
+# after connection use rdesktop or krdc to connect with rdp protocol to localhost
+ssh -f $RUSER@$RHOST -L $LPORT:$REMOTE:$RPORT -N
+
+
